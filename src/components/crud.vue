9f3029a94e56201754225b6b42b5c7eb1992fbd4
Deepankar Mondal <hi@deepankarmondal.com>
2018-02-20 16:51:36 +0530

    url replaced with live url

--- src/components/crud.vue
+++ src/components/crud.vue
@@ -36,20 +36,28 @@
               <th>#</th>
               <th >Name</th>
               <th >Contact</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-if="listData.length > 0">
-              <th>1</th>
-              <td align="left">
-                <button type="button" class="btn btn-link btn-sm pull-right" data-toggle="modal" data-target="#edit"> Mark </button>
-              </td>
-              <td align="left">
-                <button type="button" class="btn btn-link btn-sm pull-right" data-toggle="modal" data-target="#edit"> Mark </button>
-              </td>
-            </tr>
+            <template v-if="listData.length > 0">
+              <tr v-for="(value,index) in listData">
+                <th> {{ 1 + index}}</th>
+                <td align="left">
+                  <button type="button" class="btn btn-link btn-sm pull-right" data-toggle="modal" data-target="#edit" @click="editDataSet(index)"> {{ value.name}} </button>
+                </td>
+                <td align="left">
+                    {{ value.contact}}
+                </td>
+                <td align="left">
+                    <button type="button" name="button" class="btn btn-xs btn-danger" @click="deleteTest(index)">Delete</button>
+                </td>
+              </tr>
+            </template>
+
+
             <tr v-else-if="listData.length == 0">
-                <td colspan="3" align="center"> No data</td>
+                <td colspan="4" align="center"> No data</td>
             </tr>
 
           </tbody>
@@ -63,19 +71,19 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <form class="form-inline">
+          <form class="form-inline" @submit.prevent="updateData()">
           <div class="modal-body">
 
             <label class="sr-only" for="inlineFormInput">Name</label>
-            <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name">
+            <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Name" v-model="editData.name">
 
             <label class="sr-only" for="inlineFormInputGroup">Contact  Number</label>
-            <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Contact number">
+            <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Contact number" v-model="editData.contact">
 
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary">Update</button>
           </div>
           </form>
         </div>
@@ -95,12 +103,12 @@
       }
     },
     mounted(){
-      //this.fetchData();
+      this.fetchData();
     },
     methods: {
       fetchData(){
         let vm = this;
-        axios.get('http://127.0.0.1:8000/')
+        axios.get('http://api.deepankarmondal.com/api/test')
         .then(function(response){
             vm.listData =  response.data;
             console.log(response)
@@ -108,19 +116,34 @@
       },
       createData(){
         let vm = this;
-        axios.post('http://127.0.0.1:8000/',this.create)
+        console.log(this.create);
+        axios.post('http://api.deepankarmondal.com/api/test',vm.create)
           .then(function(response){
-              console.log(response)
+              //console.log(response)
+              if(response.data.success)
+                  vm.fetchData();
           })
       },
       editDataSet(index){
         this.editData  = this.listData[index];
       },
       updateData(){
-        axios.put('http://127.0.0.1:8000/'+this.editData.id,this.editData)
+        //console.log(this.editData);
+        axios.put('http://api.deepankarmondal.com/api/test/'+this.editData.id,this.editData)
             .then(response => {
               console.log(response);
             })
+      },
+      deleteTest(index){
+        let vm = this;
+        var data = this.listData[index];
+        console.log(data);
+        axios.delete('http://api.deepankarmondal.com/api/test/'+data.id)
+        .then(response =>{
+
+          if(response.data.success)
+              vm.listData.splice(index,1);
+        })
       }
     }
   }
